d83da25d1cff092e4b39dd5f6965ee818095a4d0
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-07-23 07:58:13 -0600

    feat(domain/service): Added PetService

--- src/main/java/com/veterinaria/gestion_mascotas/domain/service/PetService.java
+++ src/main/java/com/veterinaria/gestion_mascotas/domain/service/PetService.java
@@ -1,11 +1,46 @@
 package com.veterinaria.gestion_mascotas.domain.service;
 
 
+import com.veterinaria.gestion_mascotas.domain.model.Pet;
+import com.veterinaria.gestion_mascotas.domain.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetService {
     @Autowired
-    private
+    private PetRepository petRepository;
+    @Autowired
+    private AppointmentService appointmentService;
+
+    public List<Pet> getAll(){
+        return  petRepository.getAll();
+    }
+
+    public Optional<Pet> getById(Integer mascotaId){
+        return petRepository.getById(mascotaId);
+    }
+
+    public List<Pet> getByName(String name){
+        return petRepository.getByName(name);
+    }
+
+    public List<Pet> getByEdad(int edad){
+        return petRepository.getByEdad(edad);
+    }
+
+    public Pet save(Pet pet){
+        return petRepository.save(pet);
+    }
+
+    public boolean delete(Integer mascotaId){
+        if(getById(mascotaId).isPresent()){
+            appointmentService.delete(mascotaId);
+            return true;
+        }
+        return false;
+    }
 }
