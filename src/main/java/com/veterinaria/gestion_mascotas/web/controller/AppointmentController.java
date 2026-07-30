2babacba48277178eb9d58b3d3e2ef6d1514c83b
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-07-30 00:20:53 -0600

    feat(Security): Deleted all related spring security jwt

--- src/main/java/com/veterinaria/gestion_mascotas/web/controller/AppointmentController.java
+++ src/main/java/com/veterinaria/gestion_mascotas/web/controller/AppointmentController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/appointment")
 @Tag(name = "Appointment", description = "Manage appointments in the veterinary clinic")
-@SecurityRequirement(name = "bearerAuth")
 public class AppointmentController {
 
     @Autowired
